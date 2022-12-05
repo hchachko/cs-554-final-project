560427a9f7fb165da2130ab6d626be418ebb12a7
bemin.shaker <beminshaker1@outlook.com>
2022-12-05 18:30:42 -0500

    added updateStats function

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,10 +3,40 @@ const users = mongoCollections.users;
 const bcrypt = require("bcryptjs");
 const saltRounds = 8;
 
+function checkUserCredentials(username, password, email) {
+  if (!username || !password || !email) {
+    throw "Error: You must provide a username, password, and email to create a new user.";
+  } else if (
+    typeof username != "string" ||
+    typeof password != "string" ||
+    typeof email != "string"
+  ) {
+    throw "Error: Username, password, and email must be valid strings.";
+  } else if (
+    username.trim().length == 0 ||
+    password.trim().length == 0 ||
+    email.trim().length == 0
+  ) {
+    throw "Error: Username, password, and email must be not be empty.";
+  }
+}
+
+function checkUserCredentialsLogin(username, password) {
+  if (!username || !password) {
+    throw "Error: You must provide a username, and password to create an account.";
+  } else if (typeof username != "string" || typeof password != "string") {
+    throw "Error: Username, and password must be valid strings.";
+  } else if (username.trim().length == 0 || password.trim().length == 0) {
+    throw "Error: Username, and password must be not be empty.";
+  }
+}
+
 async function createUser(username, password, email) {
   username = username.toLowerCase().trim();
   password = password.trim();
   email = email.toLowerCase().trim();
+
+  checkUserCredentials(username, password, email);
 
   const usersCollection = await users();
 
@@ -36,11 +66,9 @@ async function createUser(username, password, email) {
   return newUser;
 }
 
-//Check if user already exists for login
 async function checkUser(username, password) {
-  checkLoginCredentials(username, password);
-
   username = username.toLowerCase();
+  checkUserCredentialsLogin(username, password);
 
   const usersCollection = await users();
 
@@ -61,7 +89,40 @@ async function checkUser(username, password) {
   }
 }
 
+async function updateStats(username, game_wpm, game_won) {
+  const usersCollection = await users();
+  const userExists = await usersCollection.findOne({
+    username: username,
+  });
+
+  let old_wpm = userExists.wpm;
+  let new_wpm =
+    (old_wpm * userExists.games_played + game_wpm) /
+    (userExists.games_played + 1);
+
+  let games_won_count = userExists.games_won;
+  if (game_won === true) {
+    games_won_count = games_won_count + 1;
+  }
+
+  const newStats = {
+    wpm: new_wpm,
+    games_played: userExists.games_played + 1,
+    games_won: games_won_count,
+  };
+
+  const updatedData = await usersCollection.updateOne(
+    { username: username },
+    { $set: newStats }
+  );
+
+  if (updatedData.modifiedCount == 0) {
+    throw "Error: Sweet update was unsuccessful.";
+  }
+}
+
 module.exports = {
   createUser,
   checkUser,
+  updateStats,
 };
--- backend/seed.js
+++ backend/seed.js
@@ -1,8 +1,10 @@
 const users = require("./data/users");
 
 async function main() {
-  const user1 = await users.createUser("test1", "test123", "test1@gmail.com");
-  console.log(user1);
+  //const user1 = await users.createUser("test1", "test123", "test1@gmail.com");
+  //console.log(user1);
+
+  const update1 = await users.updateStats("test1", 200, false);
 }
 
 main();
