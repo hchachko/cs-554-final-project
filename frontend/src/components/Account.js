d7a32a874c9e7dc44e394f2e06dc60e717510c5b
Harrison Chachko <hchachko@gmail.com>
2022-12-07 13:28:23 -0500

    Added form and layout for image uploading + gitIgnore file
    - Added form for users to change their profile pic, with a button that toggles its visibility
    - Fixed CSS issue with account card being hidden
    - Added gitIgnore file so node modules and package-lock.json do not have to be removed per commit

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import SignOutButton from "./SignOutButton";
 import { AuthContext } from "../firebase/Auth";
 import { Card, CardMedia } from "@mui/material";
@@ -7,7 +7,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles({
   card: {
     maxWidth: 345,
-    height: "60vh",
+    height: "60%",
     marginLeft: "auto",
     marginRight: "auto",
     borderRadius: 20,
@@ -27,10 +27,23 @@ const useStyles = makeStyles({
   },
 });
 
-function Account() {
+function Account () {
   const { currentUser } = useContext(AuthContext);
   const classes = useStyles();
-
+  const [buttonValue, setButtonValue] = useState("Edit Profile Pic");
+  const [showForm, setShowForm] = useState(false);
+  let profilePicForm = null;
+  function ProfilePicButtonChange (){
+    if (buttonValue === "Edit Profile Pic"){
+      setButtonValue("Cancel");
+      setShowForm(true);
+    }
+    else {
+      setButtonValue("Edit Profile Pic");
+      setShowForm(false);
+    }
+  }
+  if (showForm) {
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
       <Card className={classes.card} variant="outlined">
@@ -47,7 +60,17 @@ function Account() {
               className={classes.media}
             />
           )}
-
+          <br/>
+          <form>
+            <br/>
+            <label>Image URL:&nbsp;</label>
+            <input type="text"></input>
+            <br/>
+            <br/>
+            <button type="submit">Confirm</button>
+            <button onClick={ProfilePicButtonChange}>{buttonValue}</button>
+          </form>
+          <br/>
           <Typography variant="body2" color="text.secondary">
             Email: {currentUser._delegate.email}
           </Typography>
@@ -56,6 +79,34 @@ function Account() {
       </Card>
     </Grid>
   );
+  } else {
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+        <Card className={classes.card} variant="outlined">
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {currentUser._delegate.displayName}
+            </Typography>
+            {currentUser && (
+              <CardMedia
+                component="img"
+                alt="No User Image"
+                height="140"
+                image={currentUser._delegate.photoURL}
+                className={classes.media}
+              />
+            )}
+            <br/>
+            <button onClick={ProfilePicButtonChange}>{buttonValue}</button>
+            <Typography variant="body2" color="text.secondary">
+              Email: {currentUser._delegate.email}
+            </Typography>
+          </CardContent>
+          <SignOutButton className={classes.button} />
+        </Card>
+      </Grid>
+    );
+  }
 }
 
 export default Account;
